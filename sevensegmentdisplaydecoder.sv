858a838dc989480418dfd5aaa67fee3a262b4f6b
Erika Grandy <36168874+ErikaGrandy@users.noreply.github.com>
2023-04-01 17:02:16 -0400

    Passcode Modifications & Code Cleanup / Sources
    Passcode can now only be entered in SET or TRIGGER state.
    Passcode no longer requires an extra button press, instead it checks that main system is back in IDLE.
    Added an enable for the 7 Segment Timer, to only show when counting.
    Code cleanup and citation for ADC components.

--- sevensegmentdisplaydecoder.sv
+++ sevensegmentdisplaydecoder.sv
@@ -1,22 +1,29 @@
 
 //Seven Segment Display Decoder - 9 number digits
-module sevensegmentdisplaydecoder (
+module sevensegmentdisplaydecoder (input enable,
 	input [3:0] dec,
 	output reg [1:7] ledout
 	);
 	
-	always @(dec)
-		case (dec)
-			0: ledout = 7'b0000001;
-			1: ledout = 7'b1001111;
-			2: ledout = 7'b0010010;
-			3: ledout = 7'b0000110;
-			4: ledout = 7'b1001100;
-			5: ledout = 7'b0100100;
-			6: ledout = 7'b0100000;
-			7: ledout = 7'b0001111;
-			8: ledout = 7'b0000000;
-			9: ledout = 7'b0000100;
-			default: ledout = 7'b0000001;
-		endcase
+	always @(dec) begin
+		if(enable == 1'b1) begin
+			case (dec)
+				0: ledout = 7'b0000001;
+				1: ledout = 7'b1001111;
+				2: ledout = 7'b0010010;
+				3: ledout = 7'b0000110;
+				4: ledout = 7'b1001100;
+				5: ledout = 7'b0100100;
+				6: ledout = 7'b0100000;
+				7: ledout = 7'b0001111;
+				8: ledout = 7'b0000000;
+				9: ledout = 7'b0000100;
+				default: ledout = 7'b0000001;
+			endcase
+		
+		end
+		else begin
+			ledout = 7'b1111111;
+		end
+	end
 endmodule
